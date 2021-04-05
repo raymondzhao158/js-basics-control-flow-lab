44835101883bf476eec8f530c008a8f3a7865e79
raymond zhao <raymondzhao158@gmail.com>
2021-04-05 11:50:42 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue <= 400) {
+    return 'This one is on me!';
+  }else if(someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if (someValue > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city == 'NYC') {
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.' ;
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+    }
+  }
